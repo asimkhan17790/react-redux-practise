8fdf1a1c0f1c047f3faa13ed524ffdf5abe4854a
Asim Khan <asim.khan17790@gmail.com>
2020-08-25 00:36:34 -0400

    Added routing

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -2,24 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Profile from './components/Profile';
+import NotFound from './components/NotFound';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome Asim!! Learn React.
-        </a>
-      </header>
-    </div>
+      <main>
+      <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/home" component={Home} exact />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/profile" component={Profile} />
+                <Route component={NotFound} />
+      </Switch>
+    </main>
   );
 }
 
---
+++ react-redux-tutorial/src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import '../../App.css';
+
+export default function NotFound() {
+    return(<div>
+        <div>
+            <h3> 404 - Oops! Page Not Found</h3>
+        </div>
+    </div>);
+}
